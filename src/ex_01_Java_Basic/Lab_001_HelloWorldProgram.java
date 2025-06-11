e8e21f40f612a81cb42aacd399ac41e4e350a996
BMNS-Deepthi <deepthi.boina221@mail.com>
2025-06-11 23:02:30 +0530

    hello world program

---
+++ src/ex_01_Java_Basic/Lab_001_HelloWorldProgram.java
@@ -0,0 +1,6 @@
+package ex_01_Java_Basic;
+public class Lab_001_HelloWorldProgram {
+    public static void main(String[] args) {
+        System.out.println("Hello world, This is ATB13X");
+    }
+}
